35e8b0798b24804c75ea67a681a7f1f38a6cb131
nessrinemaalem <imane95-1999@live.fr>
2022-02-24 19:42:09 +0100

    encore un entrainement sur gnl et on passe a printf

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,13 @@
 /*   By: imaalem <imaalem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 15:05:11 by imaalem           #+#    #+#             */
-/*   Updated: 2022/02/23 16:12:37 by imaalem          ###   ########.fr       */
+/*   Updated: 2022/02/24 19:39:38 by imaalem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// heure de depart : 15:02
+// heure de depart : 15:02 fail -> seg auly pour des erreurs betes prnde le temps de bien faire et de relire au fur et a mesure
+
+// depart 3: 16:13
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -19,57 +21,33 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-//start: 15.05
-
 int	ft_strlen(char *str)
 {
-	int 	i = 0;
-
+	int i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-int		ft_strchr(char *str, char c)
+char	*ft_strchr(char *str, int c)
 {
-	int	i;
-	char c;
+	char *dup = (unsigned char *)str;
 
-	c = (char)str; 
 	if (!str)
 		return NULL;
-	while (str)
+	while (*dup)
 	{
-		if (c != '\n')
-			return (c);
+		if (*dup == (unsigned char)c)
+			return (dup);
+		dup++;
 	}
 	return (NULL);
-	
 }
 
-char *ft_substr(char *str, int start, int len)
+char *ft_strjoin(char *s1, char *s2)
 {
-	int 	i = 0;
-	char 	*dest;
-
-	if (!str)
-		return NULL;
-	dest = (char *)malloc(sizeof(char) * (len + 1));
-	if (!dest)
-		return NULL;
-	while (i < len)
-	{	
-		dest[i++] = str[start++];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*ft_strjoin(char* s1, char *s2)
-{
-	int		i = 0;
-	int		j = 0;
 	char	*dest;
+	int		i = 0, j = 0;
 
 	if (!s1 || !s2)
 		return NULL;
@@ -80,8 +58,8 @@ char	*ft_strjoin(char* s1, char *s2)
 	{
 		dest[i] = s1[i];
 		i++;
-	}
-	while (s2)
+	} 
+	while (s2[j])
 	{
 		dest[i + j] = s2[j];
 		j++;
@@ -91,80 +69,90 @@ char	*ft_strjoin(char* s1, char *s2)
 	return (dest);
 }
 
+char	*ft_substr(char *str, int start, int len)
+{
+	char	*dest;
+	int		i = 0;
+
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
+	if (!dest)
+		return NULL;
+	while (i < len)
+		dest[i++] = str[start++];
+	dest[i] = '\0';
+	return (dest);
+}
+
 char	*ft_read(char *tail, int fd)
 {
-	int		count = 1;
 	char 	buf[BUFFER_SIZE + 1];
+	int		count_read = 1;
 
-	while (count > 0 && !ft_strchr(tail, '\n'))
+	while (count_read > 0 && !ft_strchr(tail, '\n'))
 	{
-		printf("read\n");
-		count = read(fd, buf, BUFFER_SIZE);
+		count_read = read(fd, buf, BUFFER_SIZE);
+		if (count_read < 0)
+			return (NULL);
+		buf[count_read] = '\0';
 		if (!tail)
-			tail = ft_substr(buf, 0, count);
+			tail = ft_substr(buf, 0, count_read);
 		else
 			tail = ft_strjoin(tail, buf);
 	}
 	return (tail);
 }
 
+char	*ft_tail(char *tail, int *n)
+{
+	char *new_tail;
+
+	new_tail = ft_substr(tail, *n + 1, ft_strlen(tail));
+	free(tail);
+	return (new_tail);
+}
+
 char	*ft_line(char *tail, int *n)
 {
 	char	*line;
-	int		i = 0;
+	int		i;
 
-	while (tail[i] != '\n')
+	while (tail[i] != '\n' && tail[i])
 		i++;
-	*n = i;
 	line = ft_substr(tail, 0, i + 1);
+	*n = i;
 	return (line);
-}
-
-char *ft_tail(char *str, int *n)
-{
-	char *tail;
-
-	tail = ft_substr(str, *n + 1, ft_strlen(str));
-	free(str);
-	return (tail);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*tail;
+	static char *tail;
 	char		*line;
-	int			n;
+	int 		n = 0;
 
 	if (BUFFER_SIZE < 1 || fd < 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	tail = ft_read(tail, fd);
-	printf("test\n");
-	if (tail[0] = '\0')
+	if (!tail[0])
 	{
 		free(tail);
 		tail = NULL;
-		return (NULL);
+		return NULL;
 	}
 	line = ft_line(tail, &n);
-	printf("test1\n");
 	tail = ft_tail(tail, &n);
-	printf("test2\n");
-	return (NULL);
+	return (line);
 }
 
 int	main()
 {
-	int	fd;
-	char *str;
+	int		fd;
+	char	*str;
 
 	fd = open("file.c", O_RDONLY);
-	printf("mais\n");
 	str = get_next_line(fd);
-	printf("ou et donc or ni car\n");
 	while (str != NULL)
 	{
 		printf("%s", str);
-		free(str);
 		str = get_next_line(fd);
 	}
 	return (0);
